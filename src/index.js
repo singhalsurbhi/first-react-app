1dc31fdc62b751d1698538f9f0eb0b5394579ead
singhalsurbhi <singhalsurbhi1997@gmail.com>
2020-10-24 12:57:06 +0530

    copied the helper funtion in the end to declare winner

--- src/index.js
+++ src/index.js
@@ -11,21 +11,14 @@ function Square(props)
   );
 }
 
-// class Square extends React.Component {
-//   render() {
-//     return (
-//       <button className="square" onClick={() => this.props.onClick()}>
-//         {this.props.value}
-//       </button>
-//     );
-//   }
-// }
 
 class Board extends React.Component {
-  constructor(props)
-  {
+  constructor(props){
     super(props);
-    this.state = {Squares: Array(9).fill(null), xIsNext = true,};
+    this.state = {
+      Squares: Array(9).fill(null),
+      xIsNext : true,
+    };
   }
   
   handleClick(i)
@@ -48,7 +41,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: '+ (this.state.xIsNext? 'X' : 'O');
 
     return (
       <div>
@@ -95,3 +88,23 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
